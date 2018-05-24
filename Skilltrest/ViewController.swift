5a4ab19d0bc015a0aef2b9761093611c7bfdc4fd
Aghasomwan uyi <uyiaghas@yahoo.com>
2018-05-24 03:29:45 -0400

    clients, payments, skills, premuim

--- Skilltrest/ViewController.swift
+++ Skilltrest/ViewController.swift
@@ -10,10 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+   
+    @IBOutlet var pageControl: UIPageControl!
+    @IBOutlet var imgView: UIImageView!
+    
+    var timer : Timer!
+    var updateCounter : Int!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        updateCounter = 0
+        
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector:#selector(ViewController.updateTimer), userInfo: nil, repeats: true)
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    @objc internal func updateTimer()
+    {
+        if(updateCounter <= 2)
+        {
+            pageControl.currentPage = updateCounter
+            imgView.image = UIImage(named: String(updateCounter + 1) + ".png")
+            updateCounter = updateCounter + 1
+        }
+        else
+        {
+            updateCounter = 0
+        }
+        // run the application
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
